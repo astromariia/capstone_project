62d04d3f15e730ceebe7148316d81833b10f0b43
astromariia <mariia.shevchenko13@gmail.com>
2025-09-07 14:06:33 -0500

    Still working on color detection of purple, need to play around with values.

--- computervision.py
+++ computervision.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy as np
+from PIL import Image
 
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
-        print("Error: Could not open camera.")
-        exit()
+    print("Error: Could not open camera.")
+    exit()
 
-lower_purple = np.array([295, 45, 100])
-upper_purple = np.array([295, 100, 25])
+lower_purple = np.array([120, 100, 100])
+upper_purple = np.array([160, 255, 255])
 
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
 while True:
     ret, frame = cap.read()
@@ -18,13 +22,24 @@ while True:
         break
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    blurred_frame = cv2.GaussianBlur(hsv_frame, (21, 21), 0)
 
-    purple_mask = cv2.inRange(hsv_frame, lower_purple, upper_purple)
+    purple_mask = cv2.inRange(blurred_frame, lower_purple, upper_purple)
 
     purple_detection = cv2.bitwise_and(frame, frame, mask=purple_mask)
+    
+    #contours, _ = cv2.findContours(purple_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask_ = Image.fromarray(purple_mask)
 
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    #cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
     cv2.imshow("Original Frame", frame)
-    cv2.imshow("Purple Detection", purple_detection)
+    # cv2.imshow("Purple Detection", purple_detection)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
